0e2784ceb7b9d8f13c74afb6c2ea51d4ccf91392
绅士喵呜 <2234763217@qq.com>
2021-03-23 11:57:19 +0800

    chore: add emotion/react

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+// * --------------------------------------------------------------------------- style
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
